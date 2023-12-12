7433eacec5fd8cff3a447b8bf34f76cd05c331d9
ktmeaton <ktmeaton@gmail.com>
2023-12-12 19:02:21 +0000

    day 5: part 2 wrong

--- src/day/day_5.rs
+++ src/day/day_5.rs
@@ -3,7 +3,8 @@ use crate::utils;
 
 use color_eyre::eyre::{Report, Result};
 use itertools::Itertools;
-use log::info;
+use log::{debug, info};
+use std::cmp::{max, min};
 
 /// Day 5
 ///
@@ -15,49 +16,87 @@ pub fn run(part: &Part) -> Result<usize, Report> {
 
     // parse the seeds as the first source
     let lines = input.split("\n\n").filter(|l| !l.is_empty()).collect_vec();
-    let mut sources: Vec<usize> =
+    let sources: Vec<usize> =
         lines[0].split(' ').filter_map(|s| s.parse().ok()).collect();
 
-    // iterate through each mapping of source => destination
+    // convert to ranges
+    let mut sources = match *part {
+        Part::Part1 => sources.into_iter().map(|n| n..n).collect_vec(),
+        Part::Part2 => {
+            sources.chunks(2).into_iter().map(|v| v[0]..v[0] + v[1]).collect_vec()
+        }
+    };
+
+    // iterate through each mapping of range(source) => range(destination)
     lines[1..].iter().for_each(|line| {
         let line_split = line.split(" map:\n").collect_vec();
 
-        let lookup = line_split[1]
+        debug!("{}", "-".repeat(80)); 
+        debug!("connection: {:?}", line_split[0]);
+
+        sources.sort_by(|a, b| a.start.cmp(&b.start));        
+        debug!("\tsources: {sources:?}");
+
+        // parsing the mapping of source to new destination
+        let maps = line_split[1]
             .split('\n')
             .map(|set| {
                 set.split(' ').filter_map(|c| c.parse::<usize>().ok()).collect_vec()
             })
             .collect_vec();
 
-        let source_ranges = lookup.iter().map(|l| (l[1]..l[1] + l[2])).collect_vec();
-        let dest_ranges = lookup.iter().map(|l| (l[0]..l[0] + l[2])).collect_vec();
+        let map_sources = maps.iter().map(|l| (l[1]..l[1] + l[2])).collect_vec();
+        let map_destinations = maps.iter().map(|l| (l[0]..l[0] + l[2])).collect_vec();
+        debug!("\tmap: {map_sources:?} => {map_destinations:?}");
 
-        let destinations = sources
-            .iter()
-            .map(|n| {
-                let result =
-                    source_ranges.iter().enumerate().find(|(_i, r)| r.contains(n));
-                match result {
-                    Some((i, s)) => {
-                        let d = &dest_ranges[i];
-                        let s_min = s.clone().min().unwrap();
-                        let d_min = d.clone().min().unwrap();
-                        let conversion = d_min as isize - s_min as isize;
-                        ((*n as isize) + conversion) as usize
-                    }
-                    None => *n,
+        // split up source ranges based on available source mapping
+        let mut sources_split = sources.clone().into_iter().flat_map(|s| {
+            let matches = map_sources.iter().filter(|r| r.contains(&s.start) || r.contains(&(s.end- 1))).collect_vec();
+            let overlaps = matches.iter().map(|m| max(s.start, m.start)..min(s.end, m.end)).collect_vec();
+            //debug!("\ts: {s:?}, overlaps: {overlaps:?}");
+            if overlaps.is_empty() || overlaps == vec![s.clone()] {
+                vec![s]
+            } else {
+                let mut uniq_start = overlaps.iter().filter_map(|o| (o.start > s.start).then_some(s.start..o.start)).collect_vec();
+                let mut uniq_end = overlaps.iter().filter_map(|o| (o.end < s.end).then_some(o.end..s.end)).collect_vec();
+                //debug!("\t\tuniq_start: {uniq_start:?}, uniq_end: {uniq_end:?}");
+                let mut ranges = overlaps;
+                ranges.append(&mut uniq_start);
+                ranges.append(&mut uniq_end);
+                ranges
+            }
+        })
+        .unique()
+        .collect_vec();
+        sources_split.sort_by(|a, b| a.start.cmp(&b.start));
+        debug!("\tsources_split: {sources_split:?}");       
+
+        let destinations = sources_split
+            .into_iter()
+            .map(|s| {
+                debug!("\tsource: {s:?}");
+                let result = map_sources.iter().enumerate().find(|(_i, r)| r.contains(&s.start) && r.contains(&(s.end-1)));
+
+                if let Some((i, map_s)) = result {
+                    let map_d = &map_destinations[i];
+                    let conversion = map_d.start as isize - map_s.start as isize;
+                    debug!("\t\tmap_s: {map_s:?} => map_d: {map_d:?}, conversion: {conversion:?}");    
+                    let start = ((s.start as isize) + conversion) as usize;
+                    let end = ((s.end as isize) + conversion) as usize;
+                    start..end
+                }
+                else {
+                    s
                 }
             })
             .collect_vec();
+        debug!("\tdestinations: {destinations:?}");
         sources = destinations;
     });
 
     let destinations = sources;
 
-    let result = match *part {
-        Part::Part1 => destinations.into_iter().min().unwrap(),
-        Part::Part2 => 2,
-    };
+    let result = destinations.into_iter().map(|r| r.start).min().unwrap();
 
     info!("Answer: {result}");
     Ok(result)
