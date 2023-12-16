234456aafb66db9a0926b9326bc4b4afa6277aa1
ktmeaton <ktmeaton@gmail.com>
2023-12-16 19:55:59 +0000

    day 9: part 2 complete

--- src/day/day_9.rs
+++ src/day/day_9.rs
@@ -1,9 +1,9 @@
 use crate::day::Part;
 use crate::utils;
-// use crate::utils::table::Table;
-// use log::debug;
+use crate::utils::table::Table;
 use color_eyre::eyre::{Report, Result};
 use itertools::Itertools;
+use log::debug;
 use log::info;
 
 /// Day 9 - Oasi
@@ -21,7 +21,15 @@ pub fn run(part: &Part) -> Result<usize, Report> {
                     vec![Some(n), None::<isize>]
                 })
                 .collect_vec();
-            v.push(None);
+
+            // expand out to left and right
+            match *part {
+                Part::Part1 => v.push(None),
+                Part::Part2 => {
+                    v.insert(0, None);
+                    v.insert(0, None);
+                }
+            };
 
             let mut rows = vec![v.clone()];
             let mut y = 0;
@@ -46,56 +54,76 @@ pub fn run(part: &Part) -> Result<usize, Report> {
                 // on the final row, extrapolate on element out
                 //rows.push(next_row);
                 y += 1;
-                if diffs.len() == 1 {
-                    // extrapolate out the final row
-                    let last_x = max_x - next_row.iter().rev().position(|r| r.is_some()).unwrap();
-                    next_row[last_x + 2] = diffs[0];
-                }
                 rows.push(next_row);
                 if diffs.len() == 1 {
                     break;
                 }
             }
 
+            // extrapolate out bottom row: part 1 => R, L <= part 2
+            match *part {
+                Part::Part1 => {
+                    let last_x = max_x - rows[y].iter().rev().position(|r| r.is_some()).unwrap();
+                    rows[y][last_x + 2] = rows[y][last_x];
+                }
+                Part::Part2 => {
+                    let first_x = rows[y].iter().position(|r| r.is_some()).unwrap();
+                    rows[y][first_x - 2] = rows[y][first_x];
+                }
+            };
+
             // extrapolate
             while y > 0 {
                 let curr_row = &rows[y];
                 let mut prev_row = rows[y - 1].clone();
 
-                let curr_x = max_x - curr_row.iter().rev().position(|r| r.is_some()).unwrap();
+                let curr_x = match *part {
+                    Part::Part1 => max_x - curr_row.iter().rev().position(|r| r.is_some()).unwrap(),
+                    Part::Part2 => curr_row.iter().position(|r| r.is_some()).unwrap(),
+                };
                 let curr_n = curr_row[curr_x].unwrap();
+                let prev_n = match *part {
+                    Part::Part1 => prev_row[curr_x - 1].unwrap(),
+                    Part::Part2 => prev_row[curr_x + 1].unwrap(),
+                };
 
-                let prev_n = prev_row[curr_x - 1].unwrap();
-
-                prev_row[curr_x + 1] = Some(prev_n + curr_n);
+                match *part {
+                    Part::Part1 => prev_row[curr_x + 1] = Some(prev_n + curr_n),
+                    Part::Part2 => prev_row[curr_x - 1] = Some(prev_n - curr_n),
+                };
 
                 rows[y - 1] = prev_row;
 
                 y -= 1;
             }
 
-            // // create debug table
-            // let mut table = Table::new();
-            // table.headers = v.iter().enumerate().map(|(i, _)| i.to_string()).collect_vec();
-            // table.rows = rows.iter().map(|row| {
-            //     row.iter().map(|r| match r {
-            //         Some(n) => n.to_string(),
-            //         None => "".to_string(),
-            //     }).collect_vec()
-            // }).collect_vec();
-            // debug!("{l}\n{}", table.to_markdown()?);
+            // create debug table
+            let mut table = Table::new();
+            table.headers = v.iter().enumerate().map(|(i, _)| i.to_string()).collect_vec();
+            table.rows = rows
+                .iter()
+                .map(|row| {
+                    row.iter()
+                        .map(|r| match r {
+                            Some(n) => n.to_string(),
+                            None => "".to_string(),
+                        })
+                        .collect_vec()
+                })
+                .collect_vec();
+            debug!("{l}\n{}", table.to_markdown()?);
 
             // get final extrapolated value
-            let final_value = rows[0][max_x].unwrap();
+            let final_value = match *part {
+                Part::Part1 => rows[0][max_x].unwrap(),
+                Part::Part2 => rows[0][0].unwrap(),
+            };
             Ok(final_value)
         })
         .collect::<Result<Vec<_>, Report>>()?;
 
     // risky conversion!
-    let result = match *part {
-        Part::Part1 => extrapolated_values.iter().sum::<isize>() as usize,
-        Part::Part2 => 2,
-    };
+    let result = extrapolated_values.iter().sum::<isize>() as usize;
 
     info!("Answer: {result}");
     Ok(result)
@@ -111,7 +139,7 @@ fn part_1() -> Result<(), Report> {
 
 #[test]
 fn part_2() -> Result<(), Report> {
-    let expected = 2;
+    let expected = 948;
     let observed = run(&Part::Part2)?;
     assert_eq!(observed, expected);
     Ok(())
