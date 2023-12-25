6ea516e628380d29ca8c237e9e5abe37ecbacb98
ktmeaton <ktmeaton@gmail.com>
2023-12-25 06:36:07 +0000

    day 12: part 1 complete

--- src/day/day_12.rs
+++ src/day/day_12.rs
@@ -13,61 +13,41 @@ pub fn local_locations(springs: &str, operational: &[usize]) -> Vec<Vec<RangeInc
         .iter()
         .enumerate()
         .map(|(i, o)| {
-            debug!("i: {i}, o: {o}");
-
+            //debug!("i: {i}, o: {o}");
             // find the operational springs '#' or '?'
             // a specific number of times consecutive (o)
             // negative lookahead and negative lookbehind for '#'
-            // let mut patterns = format!("(?<!#)([#|\\?]{{{o}}})(?!\\#)");
-            // // At the beginning, no spring may come before
-            // if i == 0 { patterns = "(?<!.*#.*)".to_string() + &patterns}
-            // // In the middle, # must come before and after
-            // // if i != 0 && i != operational.len() - 1 {
-            // //     patterns = "(?<=.*[\\?|#]+.*)".to_string() + &patterns + "(?=.*[\\?|#]+.*)";
-            // // }
-            // // At the end, no spring may come after
-            // if i == operational.len() - 1 { patterns += "(?!.*#.*)"}
-
             let mut patterns = format!("[#|\\?]{{{o}}}");
             // no operational spring directly before or after
             patterns = "(?<!#)".to_string() + &patterns + "(?!\\#)";
+            // At the beginning, no springs may come before
+            patterns = if i == 0 {
+                "(?<!.*#.*)".to_string() + &patterns
+            }
             // N spring(s) must come before
-            if i > 0 {
-                let before = i + operational[0..i].iter().sum::<usize>();                
-                debug!("\tbefore: {before}");
-                patterns = format!("(?<=.{{{before},}})") + &patterns;
+            else {
+                let before = i + operational[0..i].iter().sum::<usize>();
+                format!("(?<=.{{{before},}})") + &patterns
+            };
+
+            // At the end, no springs may come after
+            patterns = if i == operational.len() - 1 {
+                patterns + "(?!.*#.*)"
             }
             // N springs must come after
-            if i < operational.len() - 1 {
-                let after = (operational.len() - 2 - i) + operational[i+1..].iter().sum::<usize>();
-                patterns += format!("(?=.{{{after},}})").as_str();
-            }
-
-            // // At the beginning, no spring may come before
-            // if i == 0 {
-            //     patterns = "(?<!.*#.*)".to_string() + &patterns
-            // }
-            // // In the middle, i spring(s) MUST come before AND after
-            // if i != 0 && i != operational.len() - 1 {
-            //     // numeric filtering
-            //     let num_broken_before = operational[0..i].iter().sum::<usize>();
-            //     patterns = format!("(?<=.*[\\?|#]{{{num_springs_before},}}.*)")
-            //         + &patterns
-            //         + "(?=.*[\\?|#]+.*)";
-            //         patterns = 
-            // }
-            // // At the end, no spring may come after
-            // if i == operational.len() - 1 {
-            //     patterns += "(?!.*#.*)"
-            // }
+            else {
+                let after =
+                    (operational.len() - 1 - i) + operational[i + 1..].iter().sum::<usize>();
+                patterns + format!("(?=.{{{after},}})").as_str()
+            };
 
             // wrap full expression in a lookahead, to get overlaps
             patterns = "(?=".to_string() + &patterns + ")";
 
-            debug!("\tpatterns: {patterns:?}");
+            //debug!("\tpatterns: {patterns:?}");
             // construct the regular expression to find these patterns
             let re = Regex::new(&patterns).unwrap();
-            let matches = re.find_iter(&springs);
+            let matches = re.find_iter(springs);
             let positions = matches
                 // convert to vector (6..9) = [6,7,8,9]
                 .map(|m| (m.0..m.0 + o).collect_vec())
@@ -75,7 +55,7 @@ pub fn local_locations(springs: &str, operational: &[usize]) -> Vec<Vec<RangeInc
                 .flat_map(|v| v.windows(*o).map(|o| o.to_owned()).collect_vec())
                 // convert sliding windows to range inclusive
                 .map(|v| v.iter().min().copied().unwrap()..=v.iter().max().copied().unwrap())
-                .inspect(|v| debug!("\tv: {v:?}"))
+                //.inspect(|v| debug!("\tv: {v:?}"))
                 .unique()
                 .collect_vec();
             positions
@@ -90,7 +70,11 @@ pub fn local_locations(springs: &str, operational: &[usize]) -> Vec<Vec<RangeInc
 ///     [1..=1, 6..=6, 10..=12],
 ///     [2..=2, 5..=5, 10..=12],
 ///     [2..=2, 6..=6, 10..=12],
-pub fn global_locations(local: &[Vec<RangeInclusive<usize>>]) -> Vec<Vec<RangeInclusive<usize>>> {
+pub fn global_locations(
+    local: &[Vec<RangeInclusive<usize>>],
+    known: &[usize],
+) -> Vec<Vec<RangeInclusive<usize>>> {
+    // check location of known springs, these must be acounted for
     let mut combinations = Vec::new();
 
     for i in 0..local.len() {
@@ -108,59 +92,54 @@ pub fn global_locations(local: &[Vec<RangeInclusive<usize>>]) -> Vec<Vec<RangeIn
                 curr
             })
             .collect_vec();
-
-        debug!("{i} combinations:");
-        combinations.iter().for_each(|c| {
-            debug!("\tcombination: {c:?}");
-        });
     }
 
-    combinations.iter().for_each(|c| {
-        debug!("final combination: {c:?}");
-    });
-
+    // make sure all known springs were found
     combinations
+        .into_iter()
+        .filter(|c| {
+            let k = known.iter().filter_map(|i| c.iter().find(|r| r.contains(i))).collect_vec();
+            k.len() == known.len()
+        })
+        .collect_vec()
 }
 /// Day 12 - Hot Springs
-///
-/// Part 1
-///
-/// Springs:     ".??..??...?##."
-/// Operational: [1, 1, 3]
-/// Valid Local Locations:
-///   1: [1], [2], [5], [6] = 4 locations
-///   1: [1], [2], [6], [6] = 4 locations
-///   3: [10, 11, 12] = 1 locations
-/// Valid Global Locations:
-///   [1], [5], [10,11,12]
-///   [2], [6], [10,11,12]
-pub fn run(part: &Part) -> Result<usize, Report> {
-    // let input = utils::read_to_string("data/day_12.txt")?;
-    // let result = input.split("\n").map(|l| {
-    //     let split = l.split(" ").collect_vec();
-    //     let operational = split[1].split(",").filter_map(|s| s.parse::<usize>().ok()).collect_vec();
-    //     let springs = split[0];
-    //     debug!("springs: {springs:?}, operational: {operational:?}");
-    //     let local = local_locations(&springs, &operational);
-    //     debug!("\tlocal: {local:?}");
-    //     let global = global_locations(&local);
-    //     debug!("\tglobal: {}", global.len());
-    //     global.len()
-    // }).sum();
-
-    let springs = "?#?#?#?#?#?#?#?";
-    let operational = [1, 3, 1, 6];
-    // let springs = "?#?#?#?#";
-    // let operational = [1, 3, 1];
-    debug!("springs: {springs:?}, operational: {operational:?}");
-    debug!("expected: .#.###.#.######");
-    let local = local_locations(springs, &operational);
-    local.iter().for_each(|l| {
-        debug!("local: {l:?}");
-    });
-    //let global = global_locations(&local);
-
-    let result = 0;
+pub fn run(_part: &Part) -> Result<usize, Report> {
+    let input = utils::read_to_string("data/day_12.txt")?;
+    let result = input
+        .split('\n')
+        .enumerate()
+        .map(|(i, l)| {
+            let split = l.split(' ').collect_vec();
+            let operational =
+                split[1].split(',').filter_map(|s| s.parse::<usize>().ok()).collect_vec();
+            let springs = split[0];
+            debug!("i: {i}, springs: {springs:?}, operational: {operational:?}");
+            let local = local_locations(springs, &operational);
+            debug!("\tlocal:");
+            local.iter().for_each(|l| {
+                debug!("\t\t{l:?}");
+            });
+            let known = springs
+                .chars()
+                .enumerate()
+                .filter_map(|(i, c)| (c == '#').then_some(i))
+                .collect_vec();
+            let global = global_locations(&local, &known);
+            debug!("\tglobal: {}", global.len());
+            debug!("\t\toriginal: {springs}");
+            global.iter().for_each(|pos| {
+                let mut solution = vec!['.'; springs.len()];
+                pos.iter().for_each(|r| {
+                    r.clone().for_each(|i| {
+                        solution[i] = '#';
+                    });
+                });
+                debug!("\t\tsolution: {}", solution.iter().join(""));
+            });
+            global.len()
+        })
+        .sum();
 
     info!("Answer: {result}");
     Ok(result)
@@ -176,7 +155,7 @@ fn part_1() -> Result<(), Report> {
 
 #[test]
 fn part_2() -> Result<(), Report> {
-    let expected = 2;
+    let expected = 7633;
     let observed = run(&Part::Part2)?;
     assert_eq!(observed, expected);
     Ok(())
